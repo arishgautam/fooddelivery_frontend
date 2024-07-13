bdae3fb30d723fba18c10884215a889ca4e44e13
arishgautam <arishgautam686@gmail.com>
2024-07-13 14:32:11 +0545

    deployed

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://fooddelivery-backend-5aua.onrender.com";
+  // const url = "http://localhost:4000";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
