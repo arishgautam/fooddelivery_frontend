e8f690df688ce4d4d0f9260b3093112ec53d8f91
arishgautam <arishgautam686@gmail.com>
2024-07-15 20:00:13 +0545

    changed

--- src/components/Fooditem/Fooditem.jsx
+++ src/components/Fooditem/Fooditem.jsx
@@ -1,45 +1,61 @@
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./Fooditem.css";
+import PropTypes from "prop-types";
+import { StoreContext } from "../../context/StoreContext";
 
-import { useContext} from 'react';
-import { assets } from '../../assets/assets'
-import './Fooditem.css'
-import PropTypes from 'prop-types';
-import { StoreContext } from '../../context/StoreContext';
-
-const FoodItem = ({id,name,price,description,image}) => {
-
-
-const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext)
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart, url } =
+    useContext(StoreContext);
   return (
-    <div className='food-item'>
-<div className="food-item-img-container">
-<img className='food-item-image' src={url+"/images/"+image} alt="" />
-{!cartItems[id]
-?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
-:<div className='food-item-counter'>
-<img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-<p>{cartItems[id]}</p>
-<img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-</div>
-}
-</div>
-<div className="food-item-info">
-    <div className="food-item-name-rating">
-        <p>{name}</p>
-        <img src={assets.rating_starts} alt="" />
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img
+          className="food-item-image"
+          src={url + "/images/" + image}
+          alt=""
+        />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">Rs {price}</p>
+      </div>
     </div>
-    <p className="food-item-desc">{description}</p>
-    <p className="food-item-price">Rs {price}</p>
-</div>
-    </div>
-  )
-}
+  );
+};
 
 FoodItem.propTypes = {
   id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-  };
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
 
-export default FoodItem
+export default FoodItem;
