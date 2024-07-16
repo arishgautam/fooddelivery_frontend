9bcece157608e855378e89252f572594df545ff6
arishgautam <arishgautam686@gmail.com>
2024-07-16 08:42:02 +0545

    [Env]

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -6,13 +6,12 @@ import PropTypes from "prop-types";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
-
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          if (category==="All" || category===item.category) {
+          if (category === "All" || category === item.category) {
             return (
               <FoodItem
                 key={index}
@@ -24,7 +23,6 @@ const FoodDisplay = ({ category }) => {
               />
             );
           }
-          
         })}
       </div>
     </div>
